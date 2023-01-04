41814d9fdfc1bfc5fb13538ee6670eaed1cf6a59
sitinuraeni206 <sitinuraeni@smkwikrama.sch.id>
2023-01-04 10:14:55 +0700

    ubah title

--- resources/views/dashboard/login.blade.php
+++ resources/views/dashboard/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Untitled</title>
+    <title>Login PPDB</title>
     <link href="{{asset('assets/img/Logo.png')}}" rel="icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{asset('assets/css/main.css')}}">
